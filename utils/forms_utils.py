99b1b496e2ae144eedee3fc0e862e981bf8701c0
frenchcommando <martialren@gmail.com>
2020-07-16 19:24:02 -0400

    rounding in pdf output

--- utils/forms_utils.py
+++ utils/forms_utils.py
@@ -59,8 +59,10 @@ def fill_pdf_from_keys(file, out_file, d):
                             r = d[key]
                             if isinstance(r, float) and r == round(r):
                                 r = int(r)
+                            elif isinstance(r, float) and r != round(r, 2):
+                                r = f'{r:.2f}'
                             annotation.update(
-                                pdfrw.PdfDict(V='{}'.format(r))
+                                pdfrw.PdfDict(V=f'{r}')
                             )
     try:
         pdfrw.PdfWriter().write(out_file, template_pdf)
